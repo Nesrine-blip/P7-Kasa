ea4313818153702bb567ba25a44a5b63732a6134
Nesrine-blip <salahnesrine29@gmail.com>
2025-08-19 16:53:18 -0700

    correction of name folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/homePage";
-import About from "./pages/aboutPage";
-import Error from "./pages/errorPage";
-import Logement from "./pages/logementPage";
+import Header from "./layout/header/header.jsx";
+import Footer from "./layout/footer/footer.jsx";
+import MainContent from "./layout/MainContent/MainContent.jsx";
+import HomePage from "./pages/HomePage/HomePage.jsx";
+import LogementPage from "./pages/LogementPage/LogementPage.jsx";
+import AboutPage from "./pages/AboutPage/AboutPage.jsx";
+import ErrorPage from "./pages/ErrorPage/ErrorPage.jsx";
+import BannerAbout from "./layout/bannerAbout/bannerAbout.jsx";
 
-
-
-
-
-
-
-
+ 
 const App = () => {
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/logement" element={<LogementPage />} />
-        <Route path="/About" element={<AboutPage />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
+
+      <MainContent>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+
+          <Route path="/logement" element={<LogementPage />} />
+
+          <Route path="/about" element={<AboutPage />} />
+
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </MainContent>
+
+      
       <Footer />
     </Router>
-  )
-}
+  );
+};
+
 export default App;
